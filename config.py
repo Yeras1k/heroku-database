9f20370a339451f79fccec6e12ade546e88571c9
Yerasyl <yerasylhello@gmail.com>
2022-04-03 15:39:23 +0600

    Add base bot logic

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = "5187778246:AAEYYSb7K24ENfsoY9fg9cgyPEM8_EmdHFE"
 APP_URL = "https://lastbotyeras.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://ryvsilddprvtxv:243b526eb218310f6947e5c8e474374ea3056853644ed069ae1ae719fb4f7e08@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/dfkqkdfo1b8bae"
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import telebot
+import psycopg2
 import logging
 from config import *
 from flask import Flask, request
@@ -9,13 +10,26 @@ server = Flask(__name__)
 logger = telebot.logger
 logger.setLevel(logging.DEBUG)
 
+db_connection = psycopg2.connect(DB_URI, sslmode="require")
+db_object = db_connection.cursor()
 
+def update_messages_count(username):
+    db_object.execute(f"UPDATE users SET password = 'qwerty' WHERE name = {username}")
+    db_connection.commit()
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    user_id = message.from_user.id
     username = message.from_user.username
     bot.reply_to(message, f"Hello, {username}!")
+
+    db_object.execute(f"SELECT name FROM users WHERE id = {username}")
+    result = db_object.fetchone()
+
+    if not result:
+        db_object.execute("INSERT INTO users(name, password) VALUES (%s, %s)", (username, 'qwerty'))
+        db_connection.commit()
+
+    update_messages_count(username)
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
