5d7c48858ea4febab1aa6fe285cb7c8e5479062d
Yeras1k <102897969+Yeras1k@users.noreply.github.com>
2022-04-05 10:30:24 +0600

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def start(message):
     result = db_object.fetchone()
 
     if not result:
-        db_object.execute("INSERT INTO users(id, name, stickers) VALUES (%s, %s, %s)", (user_id, username, 0))
+        db_object.execute("INSERT INTO users(id, username, stickers) VALUES (%s, %s, %s)", (user_id, username, 0))
         db_connection.commit()
 
 
