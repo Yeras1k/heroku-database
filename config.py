6a6c11bc5fba3bad566c8a754ae2b67ef5587d8d
Yeras1k <102897969+Yeras1k@users.noreply.github.com>
2022-04-12 10:53:37 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "5187778246:AAEYYSb7K24ENfsoY9fg9cgyPEM8_EmdHFE"
 APP_URL = "https://lastbotyeras.herokuapp.com/" + BOT_TOKEN
-DB_URI = "postgres://ryvsilddprvtxv:243b526eb218310f6947e5c8e474374ea3056853644ed069ae1ae719fb4f7e08@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/dfkqkdfo1b8bae"
+DB_URI = "postgres://ymseiahbilcjhm:c9e9009d22bceb29792fd5e613747a832dc0bebbf9a36dcb9fbbc58ccf24bc5e@ec2-54-75-184-144.eu-west-1.compute.amazonaws.com:5432/dapqqrf6l5um25"
