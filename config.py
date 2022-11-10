2354d31abc4f36ca6597945c4cdc9ee27d875f92
Yeras1k <yerasylhello@gmail.com>
2022-11-10 07:58:26 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "5187778246:AAEYYSb7K24ENfsoY9fg9cgyPEM8_EmdHFE"
-APP_URL = "https://web-production-e1bd.up.railway.app/" + BOT_TOKEN
+APP_URL = "https://web-production-b37f.up.railway.app/" + BOT_TOKEN
 DB_URI = "postgres://ymseiahbilcjhm:c9e9009d22bceb29792fd5e613747a832dc0bebbf9a36dcb9fbbc58ccf24bc5e@ec2-54-75-184-144.eu-west-1.compute.amazonaws.com:5432/dapqqrf6l5um25"
